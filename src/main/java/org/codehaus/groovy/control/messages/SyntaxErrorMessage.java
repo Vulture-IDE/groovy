809c9f729460f782255865e1ede0da11411838c1
Deenu488 <deenbandhunetam488@gmail.com>
2024-05-28 05:34:33 +0000

    fix error line

--- src/main/java/org/codehaus/groovy/control/messages/SyntaxErrorMessage.java
+++ src/main/java/org/codehaus/groovy/control/messages/SyntaxErrorMessage.java
@@ -19,6 +19,8 @@
 package org.codehaus.groovy.control.messages;
 
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.codehaus.groovy.control.Janitor;
 import org.codehaus.groovy.control.SourceUnit;
 import org.codehaus.groovy.syntax.SyntaxException;
@@ -29,14 +31,71 @@ public class SyntaxErrorMessage extends Message {
   protected SourceUnit source;
 
   public SyntaxErrorMessage(SyntaxException cause, SourceUnit source) {
-    this.cause = cause;
     this.source = source;
+
+    String message = cause.getMessage();
+    boolean isEOF = message.contains("EOF");
+
+    String errorLineString = this.source.getSource().getLine(cause.getLine(), new Janitor());
+    String errorName = null;
+    int start = -1;
+    int end = -1;
+
+    if (!isEOF) {
+      Matcher matcher = Pattern.compile("'(.*?)'").matcher(message);
+
+      if (matcher.find()) {
+        errorName = matcher.group(1);
+      }
+
+      start = errorLineString.indexOf(errorName) + 1;
+      end = start + errorName.length();
+
+      if (cause.getStartColumn() != start || cause.getEndColumn() != end) {
+        cause.setStartColumn(start);
+        cause.setEndColumn(end);
+        cause.setEndLine(end);
+      }
+    } else {
+      errorName = errorLineString.trim();
+      if (errorName.isEmpty()) {
+        int line = getValidErrorLine(cause.getLine());
+        errorLineString = this.source.getSource().getLine(line, new Janitor());
+        errorName = errorLineString.trim();
+        start = errorLineString.indexOf(errorName) + 1;
+        end = start + errorName.length();
+        cause.setLine(line);
+        cause.setStartColumn(end - 1);
+        cause.setEndColumn(end);
+      } else {
+        errorLineString = this.source.getSource().getLine(cause.getLine(), new Janitor());
+        errorName = errorLineString.trim();
+        start = errorLineString.indexOf(errorName) + 1;
+        end = start + errorName.length();
+        cause.setStartColumn(end - 1);
+        cause.setEndColumn(end);
+      }
+    }
+
+    this.cause = cause;
     cause.setSourceLocator(source.getName());
   }
 
   /** Returns the underlying SyntaxException. */
   public SyntaxException getCause() {
     return this.cause;
+  }
+
+  /** Returns the valid error line for EOF SyntaxException. */
+  private int getValidErrorLine(int line) {
+    while (line >= 0) {
+      String string = this.source.getSource().getLine(line, new Janitor());
+      if (string != null && !string.trim().isEmpty()) {
+        return line;
+      }
+      line--;
+    }
+    return line;
   }
 
   /** Writes out a nicely formatted summary of the syntax error. */
