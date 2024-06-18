e8bb055ce5eba88c41a32e162e7b9fc41e3e07e8
Deenu488 <deenbandhunetam488@gmail.com>
2024-06-18 10:49:17 +0500

    update groovy version

--- src/main/java/groovyjarjaropenbeans/Transient.java
+++ src/main/java/groovyjarjaropenbeans/Transient.java
@@ -1,3 +1,12 @@
 package groovyjarjaropenbeans;
 
-public @interface Transient {}
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Transient {
+  boolean value() default true;
+}
